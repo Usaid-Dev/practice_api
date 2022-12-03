0e4719a1526e65e8b45f4c0edd874c26990cddb4
Usaid Asif <usaidasif12@gmail.com>
2022-12-03 17:06:58 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/practice_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.practice_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen_one.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ScreenOne extends StatefulWidget {
+  const ScreenOne({super.key});
+
+  @override
+  State<ScreenOne> createState() => _ScreenOneState();
+}
+
+class _ScreenOneState extends State<ScreenOne> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PRACTICE-API'),
+      ),
+      body: Column(
+        children: const [],
+      ),
+    );
+  }
+}
