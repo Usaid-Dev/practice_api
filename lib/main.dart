4ba1714df75adeff77be772663791efb0058545f
Usaid Asif <usaidasif12@gmail.com>
2022-12-05 19:01:55 +0500

    Fetched complex-Api | Model with plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practice_api/screen_two.dart';
+import 'package:practice_api/screen_three.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ScreenTwo(),
+      home: const ScreenThree(),
     );
   }
 }
