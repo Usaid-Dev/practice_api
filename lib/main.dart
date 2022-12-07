226e5ed89d733534a9941b6528b94f37ff19a09e
Usaid Asif <usaidasif12@gmail.com>
2022-12-07 19:41:30 +0500

    Fetched very complex-Api list | Model with plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practice_api/screen_four.dart';
+import 'package:practice_api/screen_five.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ScreenFour(),
+      home: const ScreenFive(),
     );
   }
 }
