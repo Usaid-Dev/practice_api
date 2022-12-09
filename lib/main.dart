614f3a3a6e25fe0d97077f62e49250fffbe365c8
Usaid Asif <usaidasif12@gmail.com>
2022-12-09 19:16:19 +0500

    Learn Post-Api | Upload Images

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practice_api/Post_Api/SignIn.dart';
+import 'package:practice_api/Post_Api/upload_image.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SignInScreen(),
+      home: const UploadImage(),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import modal_progress_hud_nsn
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ModalProgressHudNsnPlugin.register(with: registry.registrar(forPlugin: "ModalProgressHudNsnPlugin"))
 }
